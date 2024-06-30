afe002f019a18f2f7cee2e351c931d73bcc39155
unknown <nyorga>
2024-06-30 16:20:05 +0330

    add refresh command

--- README.md
+++ README.md
@@ -67,3 +67,4 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
 
 ## Postman collection
 [Postman collection url](https://www.postman.com/nyorga/workspace/blog/overview)
+
---
+++ app/Console/Commands/Refresh.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class Refresh extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Artisan::call('migrate:fresh');
+        Artisan::call('db:seed');
+    }
+}
